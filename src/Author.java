ee134f8579baa4e4f2c0160de59508ca5ccca49d
Olga Odintsova <olga.a.odintsova@gmail.com>
2024-06-23 12:41:43 +0200

    Initial homework commit

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String surname;
@@ -13,5 +15,26 @@ public class Author {
 
     public String getSurname(){
         return this.surname;
+    }
+
+    @Override
+    public String toString() {
+        return "author = '" + name + " " + surname + '\'';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        if (name == null && surname == null) {
+            throw new RuntimeException("Both author's name and surname cannot be null");
+        }
+        return Objects.hash(name, surname);
     }
 }
